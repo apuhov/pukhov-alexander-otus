494ab9c2fcb08678094c6484c4a2ca23ca15cd0f
Pukhov Alexander <pukhov@biocad.ru>
2020-07-09 20:08:51 +0300

    feature/handling summ()

--- javascript/summ.js
+++ javascript/summ.js
@@ -7,6 +7,7 @@ sum(1)(2)(3)....(n)() === 1 + 2 + 3 + ... + n
 */
 
 function summ(a) {
+    //if (typeof a === "undefined")   return 0;
     let curSumm = a;
     function f(b) {
         curSumm += b;
@@ -21,3 +22,21 @@ function summ(a) {
 //test
 console.log( summ(1)(2)(3) );
 console.log( summ(5)(-1)(8) );
+
+
+// const summ = (a) => {
+//     const func = (b) => {
+//         if (typeof b === "undefined"){
+//             return  a;
+//         }
+//         return summ (a + b);
+//     };
+//     func.valueOf = function () {
+//       return a;
+//     };
+//     return func;
+// };
+
+// //test
+// console.log( summ(1)(2)(3) );
+// console.log( summ(5)(-1)(8) );
